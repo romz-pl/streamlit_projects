e496cdc0b6d3b7fa7024438465ed8f3574ff27d7
romz <romz@wp.pl>
2024-12-19 11:02:08 +0100

    Working with columns app

--- 07-pretty/pretty.py
+++ 07-pretty/pretty.py
@@ -1,15 +1,22 @@
 import streamlit as st
-st.title("SF Trees")
+import pandas as pd
+
+st.set_page_config(layout='wide')
+st.title('SF Trees')
 st.write(
 """
 This app analyses trees in San Francisco using
 a dataset kindly provided by SF DPW.
 """
 )
+trees_df = pd.read_csv('./input/trees.csv')
+df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
+df_dbh_grouped.columns = ['tree_count']
+
 col1, col2, col3 = st.columns(3)
 with col1:
-    st.write("Column 1")
+    st.line_chart(df_dbh_grouped)
 with col2:
-    st.write("Column 2")
+    st.bar_chart(df_dbh_grouped)
 with col3:
-    st.write("Column 3")
+    st.area_chart(df_dbh_grouped)
